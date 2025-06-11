bf140ef3f715478783273da0673c334c638f0e8c
Yang <874125760@qq.com>
2025-06-11 16:44:57 +0800

    add r50

---
+++ projects/configs/co_dino/co_dino_5scale_lsj_r50_1x_coco_instance.py
@@ -0,0 +1,100 @@
+_base_ = [
+    'co_dino_5scale_r50_1x_coco_instance.py'
+]
+
+model = dict(with_attn_mask=False)
+load_from="co_dino_5scale_lsj_r50_3x_coco.pth"
+img_norm_cfg = dict(
+    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+
+image_size = (640, 640)
+load_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations', with_bbox=True, with_mask=True),
+    dict(
+        type='Resize',
+        img_scale=image_size,
+        ratio_range=(0.1, 2.0),
+        multiscale_mode='range',
+        keep_ratio=True),
+    dict(
+        type='RandomCrop',
+        crop_type='absolute_range',
+        crop_size=image_size,
+        recompute_bbox=True,
+        allow_negative_crop=True),
+    dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
+]
+train_pipeline = [
+    dict(type='CopyPaste', max_num_pasted=100),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels', 'gt_masks']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=image_size,
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=True),
+            dict(type='RandomFlip'),
+            dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='Collect', keys=['img'])
+        ])
+]
+data_root = 'E:/BaiduNetdiskDownload/train-seg/train/'
+dataset_type = 'CocoDataset'
+data = dict(
+    samples_per_gpu=1,
+    workers_per_gpu=2,
+    train=dict(
+        type='MultiImageMixDataset',
+        dataset=dict(
+            type=dataset_type,
+            ann_file=data_root + 'annotations/train.json',
+            img_prefix=data_root + 'images/',
+            filter_empty_gt=False,
+            pipeline=load_pipeline),
+        pipeline=train_pipeline),
+    val=dict(pipeline=test_pipeline),
+    test=dict(pipeline=test_pipeline))
+
+evaluation = dict(metric=['bbox', 'segm'])
+
+# NOTE: LSJ with copy-paste requires segmentation annotations.
+# If your training data only contains bounding boxes, please use the following code:
+
+# train_pipeline = [
+#     dict(type='LoadImageFromFile'),
+#     dict(type='LoadAnnotations', with_bbox=True),
+#     dict(
+#         type='Resize',
+#         img_scale=image_size,
+#         ratio_range=(0.1, 2.0),
+#         multiscale_mode='range',
+#         keep_ratio=True),
+#     dict(
+#         type='RandomCrop',
+#         crop_type='absolute_range',
+#         crop_size=image_size,
+#         recompute_bbox=True,
+#         allow_negative_crop=True),
+#     dict(type='FilterAnnotations', min_gt_bbox_wh=(1e-2, 1e-2)),
+#     dict(type='RandomFlip', flip_ratio=0.5),
+#     dict(type='Pad', size=image_size, pad_val=dict(img=(114, 114, 114))),
+#     dict(type='Normalize', **img_norm_cfg),
+#     dict(type='DefaultFormatBundle'),
+#     dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+# ]
+# data = dict(
+#     samples_per_gpu=2,
+#     workers_per_gpu=2,
+#     train=dict(filter_empty_gt=False, pipeline=train_pipeline),
+#     val=dict(pipeline=test_pipeline),
+#     test=dict(pipeline=test_pipeline))
